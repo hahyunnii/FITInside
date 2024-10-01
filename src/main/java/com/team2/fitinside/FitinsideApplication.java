5cb6ee9a7c820498b384a09b7ad7ee6ef1001e19
[CLOUD4] 허수빈 <anmedi3623@naver.com>
2024-10-01 14:10:53 +0900

    Initial commit

---
+++ src/main/java/com/team2/fitinside/FitinsideApplication.java
@@ -0,0 +1,13 @@
+package com.team2.fitinside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitinsideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitinsideApplication.class, args);
+	}
+
+}
