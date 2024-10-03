b02cf14348dc9903c2c48ad365bfcc875ff92432
[CLOUD4] 이하현 <lovehh329@naver.com>
2024-10-03 14:36:48 +0900

    feat : product 기본틀 세팅, entity 작성

---
+++ src/main/java/com/team2/fitinside/product/controller/ProductController.java
@@ -0,0 +1,4 @@
+package com.team2.fitinside.product.controller;
+
+public class ProductController {
+}
---
+++ src/main/java/com/team2/fitinside/product/entity/ProductThumbnail.java
@@ -0,0 +1,34 @@
+package com.team2.fitinside.product.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Table(name = "product_thumbnail")
+@EntityListeners(AuditingEntityListener.class)
+public class ProductThumbnail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "image_path", nullable = false)
+    private String imagePath;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
