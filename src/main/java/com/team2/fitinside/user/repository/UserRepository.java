07d16f90f2e3c7358efe37790e2a7ebe30dce2ae
[CLOUD4] 박진영 <wlsdud2528@naver.com>
2024-10-03 14:32:01 +0900

    feat: create user entity, repository

---
+++ src/main/java/com/team2/fitinside/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.team2.fitinside.user.repository;
+
+import com.team2.fitinside.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
