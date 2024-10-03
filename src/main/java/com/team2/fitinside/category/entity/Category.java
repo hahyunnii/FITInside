b2da25e8a255ef5df3e8988c77000d532abd22d3
[CLOUD4] 유연주 <ccomi0122@gmail.com>
2024-10-03 11:11:33 +0900

    feat: category entity

---
+++ src/main/java/com/team2/fitinside/category/entity/Category.java
@@ -0,0 +1,45 @@
+package com.team2.fitinside.category.entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Column(nullable = false, length = 1000)
+    private String description;
+
+    private Long displayOrder;
+
+    // Soft delete를 위한 필드
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Category> children = new ArrayList<>();
+
+    // 삭제 시 isDeleted를 true로 설정하는 메서드
+    public void delete() {
+        this.isDeleted = true;
+    }
+
+
+}
