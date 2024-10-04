76f428dee470a9922489ffe63a1c111f9680f695
[CLOUD4] 허수빈 <anmedi3623@naver.com>
2024-10-04 21:51:38 +0900

    feat: orderDTO 생성

---
+++ src/main/java/com/team2/fitinside/order/dto/OrderStatusUpdateRequestDto.java
@@ -0,0 +1,13 @@
+package com.team2.fitinside.order.dto;
+
+import com.team2.fitinside.order.common.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderStatusUpdateRequestDto { // 주문 상태 수정 DTO
+
+    private String status;
+
+}
