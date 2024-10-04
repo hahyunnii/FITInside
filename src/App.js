ef31d583f4cfb93c674482be06301ebd88275585
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-10-04 14:56:04 +0900

    feat: 기본 레이아웃 구성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Header from './fragments/Header';
+import Footer from './fragments/Footer';
+import GuestCart from "./cart/GuestCart";
 
-function App() {
-  return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div id="root"> {/* id="root" 추가 */}
+            <Header />
+            <main className="flex-grow-1"> {/* 메인 콘텐츠를 포함하는 div */}
+                {/* 여기에 메인 콘텐츠를 추가 */}
+                <GuestCart></GuestCart>
+            </main>
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
