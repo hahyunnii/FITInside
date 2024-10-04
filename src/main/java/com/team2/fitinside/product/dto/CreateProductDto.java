484d929dd19e06df8683f81e636fc6e88f9a11ff
[CLOUD4] 이하현 <lovehh329@naver.com>
2024-10-04 12:33:26 +0900

    feat : ProductDto 초안작성

---
+++ src/main/java/com/team2/fitinside/product/dto/CreateProductDto.java
@@ -0,0 +1,15 @@
+package com.team2.fitinside.product.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CreateProductDto {
+    private String productType;
+    private String productName;
+    private int price;
+    private String info;
+    private String manufacturer;
+    private int categoryId;
+}
