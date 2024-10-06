8fada586e563e55fdf395086dff1673c4d3290f5
[CLOUD4] 허수빈 <anmedi3623@naver.com>
2024-10-07 00:17:39 +0900

    refactor: ResponseEntity 빌더 패턴으로 수정

--- src/main/java/com/team2/fitinside/order/controller/OrderAdminController.java
+++ src/main/java/com/team2/fitinside/order/controller/OrderAdminController.java
@@ -2,7 +2,6 @@ package com.team2.fitinside.order.controller;
 
 import com.team2.fitinside.order.dto.OrderResponseDto;
 import com.team2.fitinside.order.dto.OrderStatusUpdateRequestDto;
-import com.team2.fitinside.order.exception.OrderNotFoundException;
 import com.team2.fitinside.order.service.OrderAdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,41 +17,26 @@ public class OrderAdminController {
 
     private final OrderAdminService orderAdminService;
 
-    // 주문 전체 조회
     @GetMapping
     public ResponseEntity<?> findAllOrdersByAdmin() {
         List<OrderResponseDto> response = orderAdminService.findAllOrdersByAdmin();
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    // 주문 상태 수정 - 굳이 dto로 안 보여줘도 될 것 같은데.. 어차피 주문 목록 바로 띄우지않나?
+    // 굳이 dto로 안 보여줘도 될 것 같은데.. 나중에 확인
     @PatchMapping("/{order_id}/status")
     public ResponseEntity<?> updateStatusOrder(
             @PathVariable("order_id") Long orderId,
-            @RequestBody OrderStatusUpdateRequestDto request) {
+            @RequestBody OrderStatusUpdateRequestDto request) throws Exception {
 
-        try {
-            OrderResponseDto response = orderAdminService.updateOrderStatus(orderId, request);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } catch (OrderNotFoundException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND); // 404
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
+        OrderResponseDto response = orderAdminService.updateOrderStatus(orderId, request);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    // 주문 삭제
     @DeleteMapping("/{order_id}")
-    public ResponseEntity<?> deleteOrder(@PathVariable("order_id") Long orderId) {
-        try {
-            orderAdminService.deleteOrder(orderId);
-            return new ResponseEntity<>("주문 삭제 완료. orderId: " + orderId, HttpStatus.OK);
-        } catch (OrderNotFoundException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND); // 404
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<?> deleteOrder(@PathVariable("order_id") Long orderId) throws Exception {
+        orderAdminService.deleteOrder(orderId);
+        return ResponseEntity.status(HttpStatus.OK).body("주문 삭제 완료. orderId: " + orderId);
     }
 
 }
--- src/main/java/com/team2/fitinside/order/controller/OrderController.java
+++ src/main/java/com/team2/fitinside/order/controller/OrderController.java
@@ -3,10 +3,6 @@ package com.team2.fitinside.order.controller;
 import com.team2.fitinside.order.dto.OrderDetailResponseDto;
 import com.team2.fitinside.order.dto.OrderRequestDto;
 import com.team2.fitinside.order.dto.OrderUserResponseDto;
-import com.team2.fitinside.order.exception.CartEmptyException;
-import com.team2.fitinside.order.exception.OrderModificationNotAllowedException;
-import com.team2.fitinside.order.exception.OrderNotFoundException;
-import com.team2.fitinside.order.exception.OutOfStockException;
 import com.team2.fitinside.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,90 +18,47 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    // 주문 조회
     @GetMapping("/{user_id}/orders/{order_id}")
     public ResponseEntity<?> findOrder(
             @PathVariable("user_id") Long userId,
-            @PathVariable("order_id") Long orderId) {
+            @PathVariable("order_id") Long orderId) throws Exception {
 
-        try {
-            OrderDetailResponseDto response = orderService.findOrder(userId, orderId);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } catch (OrderNotFoundException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND); // 404
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
+        OrderDetailResponseDto response = orderService.findOrder(userId, orderId);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    // 주문 전체 조회
     @GetMapping("/{user_id}/orders")
-    public ResponseEntity<?> findAllOrders(@PathVariable("user_id") Long userId) {
-        try {
-            List<OrderUserResponseDto> response = orderService.findAllOrders(userId);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<?> findAllOrders(@PathVariable("user_id") Long userId) throws Exception {
+        List<OrderUserResponseDto> response = orderService.findAllOrders(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    // 주문 생성
     @PostMapping("/{user_id}/orders")
     public ResponseEntity<?> createOrder(
             @PathVariable("user_id") Long userId,
-            @RequestBody OrderRequestDto request) {
+            @RequestBody OrderRequestDto request) throws Exception {
 
-        try {
-            OrderDetailResponseDto response = orderService.createOrder(userId, request);
-            return new ResponseEntity<>(response, HttpStatus.CREATED); // 201
-        } catch (CartEmptyException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND); // 404
-        } catch (OutOfStockException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST); // 400
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR); // 500
-        }
-
+        OrderDetailResponseDto response = orderService.createOrder(userId, request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    // 주문 수정
     @PatchMapping("/{user_id}/orders/{order_id}")
     public ResponseEntity<?> updateOrder(
             @PathVariable("user_id") Long userId,
             @PathVariable("order_id") Long orderId,
-            @RequestBody OrderRequestDto request) {
+            @RequestBody OrderRequestDto request) throws Exception {
 
-        try {
-            OrderDetailResponseDto response = orderService.updateOrder(userId, orderId, request);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } catch (OrderNotFoundException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND); // 404
-        } catch (OrderModificationNotAllowedException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST); // 400
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
+        OrderDetailResponseDto response = orderService.updateOrder(userId, orderId, request);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    // 주문 취소
     @DeleteMapping("/{user_id}/orders/{order_id}")
     public ResponseEntity<?> cancelOrder(
             @PathVariable("user_id") Long userId,
-            @PathVariable("order_id") Long orderId) {
+            @PathVariable("order_id") Long orderId) throws Exception {
 
-        try {
-            orderService.cancelOrder(userId, orderId);
-            return new ResponseEntity<>("주문 취소 완료. orderId: " + orderId, HttpStatus.OK);
-        } catch (OrderNotFoundException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND); // 404
-        } catch (OrderModificationNotAllowedException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST); // 400
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
+        orderService.cancelOrder(userId, orderId);
+        return new ResponseEntity<>("주문 취소 완료. orderId: " + orderId, HttpStatus.OK);
     }
 
 }
