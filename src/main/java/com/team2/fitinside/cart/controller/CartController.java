d56905a3880fd74dcff58ec86c37db62956e7bab
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-10-06 14:59:59 +0900

    refactor: (코드리뷰 반영) 예외 처리 중복 코드를 별도의 메서드로 분리

--- src/main/java/com/team2/fitinside/cart/controller/CartController.java
+++ src/main/java/com/team2/fitinside/cart/controller/CartController.java
@@ -3,7 +3,6 @@ package com.team2.fitinside.cart.controller;
 import com.team2.fitinside.cart.dto.CartCreateRequestDto;
 import com.team2.fitinside.cart.dto.CartResponseWrapperDto;
 import com.team2.fitinside.cart.dto.CartUpdateRequestDto;
-import com.team2.fitinside.cart.exception.CartDuplicatedException;
 import com.team2.fitinside.cart.exception.CartOutOfRangeException;
 import com.team2.fitinside.cart.service.CartService;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.AccessDeniedException;
 import java.util.NoSuchElementException;
+import java.util.concurrent.Callable;
 
 @RestController
 @RequestMapping("/api/cart")
@@ -23,67 +23,59 @@ public class CartController {
 
     @GetMapping
     public ResponseEntity<CartResponseWrapperDto> findCart() {
-        try {
+
+        return callMethodAndHandleException(() -> {
             CartResponseWrapperDto carts = cartService.findAllCarts();
             return ResponseEntity.status(HttpStatus.OK).body(carts);
-        } catch (AccessDeniedException e) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new CartResponseWrapperDto(e.getMessage()));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new CartResponseWrapperDto("장바구니 조회 중 서버 에러 발생!"));
-        }
+        }, "장바구니 조회 중 서버 에러 발생!");
     }
 
     @PostMapping
     public ResponseEntity<String> createCart(@RequestBody CartCreateRequestDto dto) {
-        try {
+
+        return callMethodAndHandleException(() -> {
             cartService.createCart(dto);
-        } catch (AccessDeniedException e) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
-        } catch (CartDuplicatedException | CartOutOfRangeException | NoSuchElementException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("장바구니 조회 중 서버 에러 발생!");
-        }
-        return ResponseEntity.status(HttpStatus.OK).body("장바구니가 추가되었습니다!");
+            return ResponseEntity.status(HttpStatus.CREATED).body("장바구니가 추가되었습니다!");
+        }, "장바구니 추가 중 서버 에러 발생!");
     }
 
     @PutMapping
     public ResponseEntity<String> updateCart(@RequestBody CartUpdateRequestDto dto) {
-        try {
+
+        return callMethodAndHandleException(() -> {
             cartService.updateCart(dto);
-        } catch (AccessDeniedException e) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
-        } catch (CartOutOfRangeException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("장바구니 조회 중 서버 에러 발생!");
-        }
-        return ResponseEntity.status(HttpStatus.OK).body("장바구니가 수정되었습니다!");
+            return ResponseEntity.status(HttpStatus.OK).body("장바구니가 수정되었습니다!");
+        }, "장바구니 수정 중 서버 에러 발생!");
     }
 
-    @DeleteMapping
-    public ResponseEntity<String> deleteCart(@RequestParam Long id) {
-        try {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCart(@PathVariable("id") Long id) {
+
+        return callMethodAndHandleException(() -> {
             cartService.deleteCart(id);
-        } catch (AccessDeniedException e) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
-        } catch (NoSuchElementException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("장바구니 조회 중 서버 에러 발생!");
-        }
-        return ResponseEntity.status(HttpStatus.OK).body("장바구니가 삭제되었습니다!");
+            return ResponseEntity.status(HttpStatus.OK).body("장바구니가 삭제되었습니다!");
+        }, "장바구니 삭제 중 서버 에러 발생!");
     }
 
     @DeleteMapping("/clear")
     public ResponseEntity<String> clearCart() {
-        try {
+
+        return callMethodAndHandleException(() -> {
             cartService.clearCart();
+            return ResponseEntity.status(HttpStatus.OK).body("장바구니가 초기화되었습니다!");
+        }, "장바구니 초기화 중 서버 에러 발생!");
+    }
+
+    // 메서드를 호출하고 공통 예외 처리 메서드
+    private <T> ResponseEntity<T> callMethodAndHandleException(Callable<ResponseEntity<T>> action, String message) {
+        try {
+            return action.call();
         } catch (AccessDeniedException e) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body((T) e.getMessage());
+        } catch (CartOutOfRangeException | NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body((T) e.getMessage());
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("장바구니 조회 중 서버 에러 발생!");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body((T) message);
         }
-        return ResponseEntity.status(HttpStatus.OK).body("장바구니가 초기화되었습니다!");
     }
 }
