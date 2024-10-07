1644c376bfbf1da9fcc469a8cc1d8ce69f52edf4
[CLOUD4] 유연주 <ccomi0122@gmail.com>
2024-10-07 11:09:56 +0900

    refactor: Category 수정 (코드리뷰)

---
+++ src/main/java/com/team2/fitinside/category/exception/CategoryNotFoundException.java
@@ -0,0 +1,8 @@
+package com.team2.fitinside.category.exception;
+
+public class CategoryNotFoundException extends RuntimeException {
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
+}
+
