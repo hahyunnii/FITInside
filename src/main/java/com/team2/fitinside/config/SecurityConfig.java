95dfd1398f7b0738f239fcb57655878f706617a1
[CLOUD4] 박진영 <wlsdud2528@naver.com>
2024-10-07 14:35:24 +0900

    feat: SecurityConfig - Swagger 설정 추가

--- src/main/java/com/team2/fitinside/config/SecurityConfig.java
+++ src/main/java/com/team2/fitinside/config/SecurityConfig.java
@@ -48,6 +48,8 @@ public class SecurityConfig{
         http.authorizeHttpRequests(auth ->
                 auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
         );
 
