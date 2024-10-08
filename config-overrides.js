07002ebe1ddf0808bb7795f11613c0cad9a610e6
[CLOUD4] 박진영 <wlsdud2528@naver.com>
2024-10-08 16:24:45 +0900

    feat: config-overrides.js 추가

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+module.exports = function override(config, env) {
+    config.resolve.fallback = {
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        util: require.resolve('util/'),
+        zlib: require.resolve('browserify-zlib'),
+        stream: require.resolve('stream-browserify'),
+    };
+    return config;
+};
