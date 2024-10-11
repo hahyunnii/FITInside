0bb1d6b08451b16e95f145a289c3ec3ae2e1da3f
[CLOUD4] 박진영 <wlsdud2528@naver.com>
2024-10-11 11:35:43 +0000

    Revert "Merge branch 'develop' into 'master'"
    This reverts merge request !20

--- src/main/java/com/team2/fitinside/order/exception/OutOfStockException.java
+++
@@ -1,7 +0,0 @@
-package com.team2.fitinside.order.exception;
-
-public class OutOfStockException extends RuntimeException {
-    public OutOfStockException(String message) {
-        super(message);
-    }
-}
