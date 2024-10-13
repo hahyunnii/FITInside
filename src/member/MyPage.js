d9e2a4d4a4ec3ecccd233add4efddcbfcc8d3e2d
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-10-13 22:23:35 +0900

    feat: mypage에 쿠폰 관리 버튼 추가

--- src/member/MyPage.js
+++ src/member/MyPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const MyPage = () => {
     const [userInfo, setUserInfo] = useState(null); // 사용자 정보를 저장할 상태
@@ -25,6 +26,12 @@ const MyPage = () => {
         fetchUserData(); // 사용자 데이터 가져오기
     }, []); // 컴포넌트가 처음 렌더링될 때 한 번만 실행
 
+    const navigate = useNavigate(); // useNavigate 훅 사용
+
+    const handleCouponManagementClick = () => {
+        navigate('/coupons'); // 쿠폰 관리 페이지로 이동
+    };
+
     return (
         <div className="container mt-5">
             <h2 className="text-center mb-4">내 정보</h2>
@@ -42,6 +49,15 @@ const MyPage = () => {
                     <div className="mb-3" style={{border: '1px solid #ddd', borderRadius: '8px', padding: '10px'}}>
                         <h5>전화번호</h5>
                         <p>{userInfo.phone}</p>
+                    </div>
+
+                    {/*내 쿠폰 관리*/}
+                    <div className="mb-3" style={{border: '1px solid #ddd', borderRadius: '8px', padding: '10px'}}>
+                        <h5>쿠폰</h5>
+                        <button className="btn btn-light text-dark mb-4" style={{border: '1px solid #ced4da'}}
+                            onClick={handleCouponManagementClick}>
+                            쿠폰 관리하기
+                        </button>
                     </div>
                 </div>
             ) : (
