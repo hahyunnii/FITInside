01e636563794c157a9a0078a2f31d88411e3a092
[CLOUD4] 유연주 <ccomi0122@gmail.com>
2024-10-14 11:56:01 +0900

    refactor: Category 수정,주석 정리

--- src/mainPage/components/MenswearPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MenswearPage = () => {
-    return <h1>남성의류 페이지</h1>;
-};
-
-export default MenswearPage; // 기본 내보내기 확인
--- src/mainPage/components/WomenswearPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const WomenswearPage = () => {
-    return <h1>여성의류 페이지</h1>;
-};
-
-export default WomenswearPage; //
