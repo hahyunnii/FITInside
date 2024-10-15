ce1972eb1101ae9765fee55670d157bee95a1cec
A540 <51090700+A540@users.noreply.github.com>
2024-10-15 14:42:12 +0900

    feat: RefreshAccessToken.js 토큰 재발급 함수 분리

--- src/member/MyPage.js
+++ src/member/MyPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import sendRefreshTokenAndStoreAccessToken from "../auth/RefeshAccessToken";
+import sendRefreshTokenAndStoreAccessToken from "../auth/RefreshAccessToken";
 
 const MyPage = () => {
     const [userInfo, setUserInfo] = useState(null); // 사용자 정보를 저장할 상태
