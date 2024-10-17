8b96313fb3b105cce43342330f5ab4634f6d06c2
A540 <51090700+A540@users.noreply.github.com>
2024-10-17 15:14:12 +0900

    feat: DeleteMemberByAdmin 관리자 권한으로 회원 정지 API 추가

--- src/main/java/com/team2/fitinside/member/dto/MemberResponseDto.java
+++ src/main/java/com/team2/fitinside/member/dto/MemberResponseDto.java
@@ -8,6 +8,8 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class MemberResponseDto {
+
+    private long id;
     private String email;
     private String userName;
     private String phone;
