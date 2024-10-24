f93192d55c5fa7b0a665623027aa2ee43172f86c
[CLOUD4] 허수빈 <anmedi3623@naver.com>
2024-10-24 11:24:40 +0900

    refactor: 사용안하는 exception 삭제

--- src/main/java/com/team2/fitinside/order/exception/OrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.team2.fitinside.order.exception;
-
-public class OrderNotFoundException extends RuntimeException {
-    public OrderNotFoundException(String message) {
-        super(message);
-    }
-}
