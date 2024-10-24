1f51c19bed143a198f1195cdf3f9694fa0cccdfa
[CLOUD4] 이하현 <lovehh329@naver.com>
2024-10-24 16:31:52 +0900

    comment: 필요없는 폴더, 주석 제거

--- src/main/java/com/team2/fitinside/product/dto/ProductResponseDto.java
+++ src/main/java/com/team2/fitinside/product/dto/ProductResponseDto.java
@@ -10,15 +10,15 @@ import java.util.List;
 public class ProductResponseDto {
 
     private Long id;                 // 상품 ID
-    private Long categoryId;
-    private String categoryName; // 카테고리 ID
+    private Long categoryId;         // 카테고리 ID
+    private String categoryName;     // 카테고리 이름
     private String productName;      // 상품명
     private int price;               // 가격
     private String info;             // 상품 설명
     private int stock;               // 재고
     private boolean isSoldOut;       // 품절여부
     private String manufacturer;     // 재조사
-    private List<String> productImgUrls; //이미지url들
+    private List<String> productImgUrls;     //이미지url들
     private List<String> productDescImgUrls; // 상품 설명 이미지들
 
 
